d9408c862732f326eea730ef4c2677ced919d6cc
DAVID <owolagbadavid@gmail.com>
2023-05-11 15:24:52 +0100

    web-sockets

---
+++ web-sockets/src/gateway/gateway.ts
@@ -0,0 +1,29 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+@WebSocketGateway()
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('connected');
+    });
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      msg: 'new message',
+      content: body,
+    });
+  }
+}
